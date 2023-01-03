d3902996fd15ff65cd3db06b12999f3761dfc1b4
grandeljay <github.jay@grandel.anonaddy.me>
2023-01-03 14:31:09 +0100

    Create grandeljay_french_language.php

---
+++ src/lang/french/modules/system/grandeljay_french_language.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * French Language
+ *
+ * @author  Jay Trees <french-language@grandels.email>
+ * @link    https://github.com/grandeljay/modified-french-language
+ * @package GrandelJayFrenchLanguage
+ */
+
+define('MODULE_GRANDELJAY_FRENCH_LANGUAGE_TITLE', 'grandeljay - Langue française');
+define('MODULE_GRANDELJAY_FRENCH_LANGUAGE_LONG_DESCRIPTION', '');
+define('MODULE_GRANDELJAY_FRENCH_LANGUAGE_STATUS_TITLE', 'Activer le pack linguistique ?');
+define('MODULE_GRANDELJAY_FRENCH_LANGUAGE_STATUS_DESC', '');
