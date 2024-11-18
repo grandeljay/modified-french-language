5d04b7ec778f05b328bb4a3e790766dd44c521fe
grandeljay <github@grandels.email>
2024-11-18 14:53:11 +0100

    chore(intelephense): add dynamically defined constants

---
+++ intelephense.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Some constants are defined dynamically, which intelephense consideres wrong.
+ * To work around this issue, you are defining the constants again in this file,
+ * just for intelephense. This file will not actually be called.
+ *
+ * @link https://github.com/bmewburn/vscode-intelephense/issues/1706#issuecomment-804843284
+ */
+
+define('FILENAME_CONFIGURATION', '');
+define('FILENAME_COUPON_ADMIN', '');
+define('FILENAME_CUSTOMERS_STATUS', '');
+define('STORE_NAME', '');
+define('STORE_OWNER_EMAIL_ADDRESS', '');
+define('ENTRY_FIRST_NAME_MIN_LENGTH', '');
+define('ENTRY_LAST_NAME_MIN_LENGTH', '');
+define('ENTRY_EMAIL_ADDRESS_MIN_LENGTH', '');
+define('ENTRY_STREET_ADDRESS_MIN_LENGTH', '');
+define('ENTRY_POSTCODE_MIN_LENGTH', '');
+define('ENTRY_CITY_MIN_LENGTH', '');
+define('ENTRY_TELEPHONE_MIN_LENGTH', '');
+define('ENTRY_PASSWORD_MIN_LENGTH', '');
+define('MODULE_ORDER_TOTAL_PAYMENT_NUMBER', '');
+define('MODULE_PAYMENT_BANKTRANSFER_URL_NOTE', '');
+define('MODULE_PAYMENT_BANKTRANSFER_IBAN_ONLY', '');
+define('MODULE_PAYMENT_EUSTANDARDTRANSFER_BANKNAM', '');
+define('MODULE_PAYMENT_EUSTANDARDTRANSFER_BRANCH', '');
+define('MODULE_PAYMENT_EUSTANDARDTRANSFER_ACCIBAN', '');
+define('MODULE_PAYMENT_EUSTANDARDTRANSFER_BANKBIC', '');
+define('MODULE_PAYMENT_EUSTANDARDTRANSFER_SUCCESS', '');
+define('_PAYMENT_MONEYBOOKERS_EMAILID', '');
+define('MODULE_PAYMENT_MONEYORDER_PAYTO', '');
+define('MODULE_PAYMENT_MONEYORDER_SUCCESS', '');
+define('MODULE_SHIPPING_AP_NUMBER_ZONES', '');
+define('MODULE_SHIPPING_DP_NUMBER_ZONES', '');
+define('MODULE_SHIPPING_FREEAMOUNT_NUMBER_ZONES', '');
+define('MODULE_SHIPPING_ITEM_NUMBER_ZONES', '');
+define('MODULE_SHIPPING_TABLE_NUMBER_ZONES', '');
+define('MODULE_SHIPPING_UPS_FREEAMOUNT', '');
+define('MODULE_SHIPPING_ZONES_NUMBER_ZONES', '');
+define('FILENAME_COOKIE_CONSENT', '');
+define('MODULE_EXCLUDE_PAYMENT_NUMBER', '');
+define('REVIEW_TEXT_MIN_LENGTH', '');
+define('ENTRY_STATE_MIN_LENGTH', '');
+define('MAX_DISPLAY_NEW_PRODUCTS_DAYS', '');
+define('STOCK_MARK_PRODUCT_OUT_OF_STOCK', '');
+define('SEARCH_MIN_LENGTH', '');
+define('MAX_PRODUCTS_QTY', '');
