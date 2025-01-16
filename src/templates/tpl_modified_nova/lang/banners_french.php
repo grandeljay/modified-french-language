9d2471af288c94f8e4f82af90b7b11a9fe7a9115
grandeljay <github@grandels.email>
2025-01-16 16:05:43 +0100

    feat(compatibility): add `tpl_modified_nova`

---
+++ src/templates/tpl_modified_nova/lang/banners_french.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * French language
+ *
+ * @author  Jay Trees <french-language@grandels.email>
+ * @link    https://github.com/grandeljay/modified-french-language
+ * @package GrandelJayFrenchLanguage
+ */
+
+  define('TEXT_BANNER_GROUP_FOR_TEMPLATE', 'Groupes de bannières pour votre template');
+  define('TEXT_RECOMMENDED_BANNER_SETTINGS', 'Paramètres de bannière recommandés pour');
+  define('TEXT_CONFIG_IMAGE_OPTIONS', 'Configuration ->; Options de l\'image');
+  define('TEXT_BANNER_IMAGES_WIDTH', 'Largeur des images de la bannière :');
+  define('TEXT_BANNER_IMAGES_HEIGHT', 'Hauteur des bannières Images :');
+  define('TEXT_BANNER_IMAGES_WIDTH_MOBILE', 'Largeur des images de la bannière Mobil :');
+  define('TEXT_BANNER_IMAGES_HEIGHT_MOBILE', 'Hauteur des images de la bannière Mobil :');
+  define('TEXT_SLIDER', 'Slider');
+  define('TEXT_SLIDER_BANNER', 'Slider / Bannière');
+  define('TEXT_BANNER_GROUP', 'Groupe de bannières :');
+  define('TEXT_WIDTH', 'Largeur');
+  define('TEXT_DESKTOP', 'Bureau :');
+  define('TEXT_MOBILE', 'Mobile :');
+  define('TEXT_BANNER_ROW_1', 'Bannière rangée 1');
+  define('TEXT_BANNER_ROW_2', 'Bannière rangée 2');
+  define('TEXT_BANNER_ROW_3', 'Bannière rangée 3');
+  define('TEXT_BANNER_ROW_4', 'Bannière rangée 4');
