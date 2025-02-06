bdb98929a8974044e89ba3c7728d8d132ac4b33c
Jay Trees <github@grandels.email>
2025-02-06 11:43:29 +0100

    chore(module): update to v1.2.2

--- src/admin/includes/modules/system/grandeljay_french_language.php
+++ src/admin/includes/modules/system/grandeljay_french_language.php
@@ -16,7 +16,7 @@ use RobinTheHood\ModifiedStdModule\Classes\StdModule;
 
 class grandeljay_french_language extends StdModule
 {
-    public const VERSION = '1.2.1';
+    public const VERSION = '1.2.2';
 
     public function __construct()
     {
