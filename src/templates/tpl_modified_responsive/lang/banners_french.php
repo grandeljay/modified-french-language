a10f89a023dd717843131c5ed4356222692bb6d8
Jay Trees <github@grandels.email>
2025-02-06 10:56:26 +0100

    fix: use source strings from `tpl_modified_responsive`

--- src/templates/tpl_modified_responsive/lang/banners_french.php
+++ src/templates/tpl_modified_responsive/lang/banners_french.php
@@ -16,12 +16,7 @@
   define('TEXT_BANNER_IMAGES_WIDTH_MOBILE', 'Largeur des images de la bannière Mobil :');
   define('TEXT_BANNER_IMAGES_HEIGHT_MOBILE', 'Hauteur des images de la bannière Mobil :');
   define('TEXT_SLIDER', 'Slider');
-  define('TEXT_SLIDER_BANNER', 'Slider / Bannière');
   define('TEXT_BANNER_GROUP', 'Groupe de bannières :');
   define('TEXT_WIDTH', 'Largeur');
   define('TEXT_DESKTOP', 'Bureau :');
   define('TEXT_MOBILE', 'Mobile :');
-  define('TEXT_BANNER_ROW_1', 'Bannière rangée 1');
-  define('TEXT_BANNER_ROW_2', 'Bannière rangée 2');
-  define('TEXT_BANNER_ROW_3', 'Bannière rangée 3');
-  define('TEXT_BANNER_ROW_4', 'Bannière rangée 4');
--- src/templates/tpl_modified_responsive/lang/buttons_french.php
+++ src/templates/tpl_modified_responsive/lang/buttons_french.php
@@ -10,144 +10,141 @@
 
 define('CSS_IMAGE_BUTTON_ADD_QUICK_TEXT', '');
 define('CSS_IMAGE_BUTTON_ADD_QUICK_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_ADD_QUICK_ICON_LEFT', '<i class="fa-solid fa-cart-shopping"></i>');
+define('CSS_IMAGE_BUTTON_ADD_QUICK_ICON_LEFT', '<i class="fas fa-shopping-cart"></i>');
 
 define('CSS_IMAGE_BUTTON_BACK_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_BACK_ICON_LEFT', '<i class="fa-solid fa-arrow-left-long"></i>');
+define('CSS_IMAGE_BUTTON_BACK_ICON_LEFT', '<i class="fas fa-caret-left"></i>');
 
-define('CSS_IMAGE_BUTTON_CONTINUE_ICON_RIGHT', '<i class="fa-solid fa-arrow-right-long"></i>');
+define('CSS_IMAGE_BUTTON_CONTINUE_ICON_RIGHT', '<i class="fas fa-caret-right"></i>');
 define('CSS_IMAGE_BUTTON_CONTINUE_ICON_LEFT', '');
 
 define('CSS_IMAGE_BUTTON_LOGIN_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_LOGIN_ICON_LEFT', '<i class="fa-solid fa-right-to-bracket"></i>');
+define('CSS_IMAGE_BUTTON_LOGIN_ICON_LEFT', '<i class="fas fa-sign-in-alt"></i>');
 
 define('CSS_IMAGE_BUTTON_SEARCH_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_SEARCH_ICON_LEFT', '<i class="fa-solid fa-magnifying-glass"></i>');
+define('CSS_IMAGE_BUTTON_SEARCH_ICON_LEFT', '<i class="fas fa-search"></i>');
 
-define('CSS_IMAGE_BUTTON_QUICK_FIND_TEXT', '');
 define('CSS_IMAGE_BUTTON_QUICK_FIND_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_QUICK_FIND_ICON_LEFT', '<i class="fa-solid fa-magnifying-glass"></i>');
+define('CSS_IMAGE_BUTTON_QUICK_FIND_ICON_LEFT', '<i class="fas fa-search"></i>');
 
 define('CSS_IMAGE_BUTTON_LOGIN_SMALL_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_LOGIN_SMALL_ICON_LEFT', '<i class="fa-solid fa-right-to-bracket"></i>');
+define('CSS_IMAGE_BUTTON_LOGIN_SMALL_ICON_LEFT', '<i class="fas fa-sign-in-alt"></i>');
 
 define('CSS_IMAGE_BUTTON_SEND_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_SEND_ICON_LEFT', '<i class="fa-regular fa-square-check"></i>');
+define('CSS_IMAGE_BUTTON_SEND_ICON_LEFT', '<i class="far fa-check-square"></i>');
 
 define('CSS_IMAGE_BUTTON_ADD_ADDRESS_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_ADD_ADDRESS_ICON_LEFT', '<i class="fa-regular fa-square-plus"></i>');
+define('CSS_IMAGE_BUTTON_ADD_ADDRESS_ICON_LEFT', '<i class="far fa-plus-square"></i>');
 
 define('CSS_SMALL_IMAGE_BUTTON_EDIT_ICON_RIGHT', '');
-define('CSS_SMALL_IMAGE_BUTTON_EDIT_ICON_LEFT', '<i class="fa-solid fa-pen-to-square"></i>');
+define('CSS_SMALL_IMAGE_BUTTON_EDIT_ICON_LEFT', '<i class="fas fa-edit"></i>');
 
 define('CSS_SMALL_IMAGE_BUTTON_DELETE_ICON_RIGHT', '');
-define('CSS_SMALL_IMAGE_BUTTON_DELETE_ICON_LEFT', '<i class="fa-regular fa-trash-can"></i>');
+define('CSS_SMALL_IMAGE_BUTTON_DELETE_ICON_LEFT', '<i class="far fa-trash-alt"></i>');
 
 define('CSS_SMALL_IMAGE_BUTTON_VIEW_ICON_RIGHT', '');
-define('CSS_SMALL_IMAGE_BUTTON_VIEW_ICON_LEFT', '<i class="fa-solid fa-magnifying-glass-plus"></i>');
+define('CSS_SMALL_IMAGE_BUTTON_VIEW_ICON_LEFT', '<i class="fas fa-search-plus"></i>');
 
 define('CSS_IMAGE_BUTTON_VIEW_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_VIEW_ICON_LEFT', '<i class="fa-solid fa-magnifying-glass-plus"></i>');
+define('CSS_IMAGE_BUTTON_VIEW_ICON_LEFT', '<i class="fas fa-search-plus"></i>');
 
 define('CSS_IMAGE_BUTTON_DELETE_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_DELETE_ICON_LEFT', '<i class="fa-regular fa-trash-can"></i>');
+define('CSS_IMAGE_BUTTON_DELETE_ICON_LEFT', '<i class="far fa-trash-alt"></i>');
 
 define('CSS_IMAGE_BUTTON_UPDATE_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_UPDATE_ICON_LEFT', '<i class="fa-solid fa-rotate"></i>');
+define('CSS_IMAGE_BUTTON_UPDATE_ICON_LEFT', '<i class="fas fa-sync-alt"></i>');
 
 define('CSS_IMAGE_BUTTON_UPDATE_CART_TEXT', 'Mettre à jour');
 define('CSS_IMAGE_BUTTON_UPDATE_CART_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_UPDATE_CART_ICON_LEFT', '<i class="fa-solid fa-rotate"></i>');
+define('CSS_IMAGE_BUTTON_UPDATE_CART_ICON_LEFT', '<i class="fas fa-sync-alt"></i>');
 
 define('CSS_IMAGE_BUTTON_WRITE_REVIEW_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_WRITE_REVIEW_ICON_LEFT', '<i class="fa-solid fa-thumbs-up"></i>');
+define('CSS_IMAGE_BUTTON_WRITE_REVIEW_ICON_LEFT', '<i class="fas fa-thumbs-up"></i>');
 
 define('CSS_IMAGE_REDEEM_GIFT_ICON_ICON_RIGHT', '');
-define('CSS_IMAGE_REDEEM_GIFT_ICON_LEFT', '<i class="fa-solid fa-ticket"></i>');
+define('CSS_IMAGE_REDEEM_GIFT_ICON_LEFT', '<i class="fas fa-ticket-alt"></i>');
 
 define('CSS_IMAGE_BUTTON_DOWNLOAD_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_DOWNLOAD_ICON_LEFT', '<i class="fa-solid fa-download"></i>');
+define('CSS_IMAGE_BUTTON_DOWNLOAD_ICON_LEFT', '<i class="fas fa-download"></i>');
 
 define('CSS_IMAGE_BUTTON_CHANGE_ADDRESS_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_CHANGE_ADDRESS_ICON_LEFT', '<i class="fa-solid fa-pen-to-square"></i>');
+define('CSS_IMAGE_BUTTON_CHANGE_ADDRESS_ICON_LEFT', '<i class="fas fa-edit"></i>');
 
 define('CSS_IMAGE_BUTTON_PRINT_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_PRINT_ICON_LEFT', '<i class="fa-solid fa-print"></i>');
+define('CSS_IMAGE_BUTTON_PRINT_ICON_LEFT', '<i class="fas fa-print"></i>');
 
 define('CSS_IMAGE_BUTTON_CHECKOUT_TEXT', 'Aller à la caisse');
-define('CSS_IMAGE_BUTTON_CHECKOUT_ICON_RIGHT', '<i class="fa-solid fa-arrow-right-long"></i>');
-define('CSS_IMAGE_BUTTON_CHECKOUT_ICON_LEFT', '<i class="fa-regular fa-credit-card"></i>');
+define('CSS_IMAGE_BUTTON_CHECKOUT_ICON_RIGHT', '<i class="fas fa-angle-right"></i>');
+define('CSS_IMAGE_BUTTON_CHECKOUT_ICON_LEFT', '<i class="fas fa-credit-card"></i>');
 
 define('CSS_IMAGE_BUTTON_DETAILS_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_DETAILS_ICON_LEFT', '<i class="fa-solid fa-magnifying-glass-plus"></i>');
+define('CSS_IMAGE_BUTTON_DETAILS_ICON_LEFT', '<i class="fas fa-search-plus"></i>');
 
 define('CSS_IMAGE_BUTTON_ADD_CART_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_ADD_CART_ICON_LEFT', '<i class="fa-solid fa-cart-shopping"></i>');
+define('CSS_IMAGE_BUTTON_ADD_CART_ICON_LEFT', '<i class="fas fa-shopping-cart"></i>');
 
 define('CSS_IMAGE_WISHLIST_DEL_TEXT', '');
 define('CSS_IMAGE_WISHLIST_DEL_ICON_RIGHT', '');
-define('CSS_IMAGE_WISHLIST_DEL_ICON_LEFT', '<i class="fa-regular fa-trash-can"></i>');
+define('CSS_IMAGE_WISHLIST_DEL_ICON_LEFT', '<i class="far fa-trash-alt"></i>');
 
-define('CSS_IMAGE_CART_DEL_TEXT', 'Supprimer');
+define('CSS_IMAGE_CART_DEL_TEXT', '');
 define('CSS_IMAGE_CART_DEL_ICON_RIGHT', '');
-define('CSS_IMAGE_CART_DEL_ICON_LEFT', '<i class="fa-regular fa-trash-can"></i>');
+define('CSS_IMAGE_CART_DEL_ICON_LEFT', '<i class="far fa-trash-alt"></i>');
 
 define('CSS_IMAGE_BUTTON_LOGIN_NEWSLETTER_TEXT', '');
 define('CSS_IMAGE_BUTTON_LOGIN_NEWSLETTER_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_LOGIN_NEWSLETTER_ICON_LEFT', '<i class="fa-regular fa-circle-right"></i>');
+define('CSS_IMAGE_BUTTON_LOGIN_NEWSLETTER_ICON_LEFT', '<i class="fas fa-chevron-circle-right"></i>');
 
 define('CSS_IMAGE_BUTTON_CONFIRM_ORDER_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_CONFIRM_ORDER_ICON_LEFT', '<i class="fa-regular fa-square-check"></i>');
+define('CSS_IMAGE_BUTTON_CONFIRM_ORDER_ICON_LEFT', '<i class="far fa-check-square"></i>');
 
-define('CSS_IMAGE_BUTTON_CHECKOUT_STEP2_ICON_RIGHT', '<i class="fa-solid fa-arrow-right-long"></i>');
+define('CSS_IMAGE_BUTTON_CHECKOUT_STEP2_ICON_RIGHT', '<i class="fas fa-caret-right"></i>');
 define('CSS_IMAGE_BUTTON_CHECKOUT_STEP2_ICON_LEFT', '');
 
-define('CSS_IMAGE_BUTTON_CHECKOUT_STEP3_ICON_RIGHT', '<i class="fa-solid fa-arrow-right-long"></i>');
+define('CSS_IMAGE_BUTTON_CHECKOUT_STEP3_ICON_RIGHT', '<i class="fas fa-caret-right"></i>');
 define('CSS_IMAGE_BUTTON_CHECKOUT_STEP3_ICON_LEFT', '');
 
-define('CSS_IMAGE_BUTTON_SAVE_TEXT', 'Enregistrer');
-define('CSS_IMAGE_BUTTON_SAVE_TITLE', 'Enregistrer');
+define('CSS_IMAGE_BUTTON_SAVE_TEXT', 'Speichern');
 define('CSS_IMAGE_BUTTON_SAVE_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_SAVE_ICON_LEFT', '<i class="fa-regular fa-floppy-disk"></i>');
+define('CSS_IMAGE_BUTTON_SAVE_ICON_LEFT', '<i class="far fa-save"></i>');
 
-define('CSS_IMAGE_BUTTON_IN_CART_TEXT', 'Ajouter au panier');
-define('CSS_IMAGE_BUTTON_IN_CART_ICON_RIGHT', '<i class="fa-solid fa-arrow-right-long"></i>');
-define('CSS_IMAGE_BUTTON_IN_CART_ICON_LEFT', '<i class="fa-solid fa-cart-shopping"></i>');
+define('CSS_IMAGE_BUTTON_IN_CART_TEXT', 'In den Warenkorb');
+define('CSS_IMAGE_BUTTON_IN_CART_ICON_RIGHT', '<i class="fas fa-angle-right"></i>');
+define('CSS_IMAGE_BUTTON_IN_CART_ICON_LEFT', '<i class="fas fa-shopping-cart"></i>');
 
-define('CSS_IMAGE_BUTTON_CHECKOUT_EXPRESS_TEXT', 'Mon achat rapide');
-define('CSS_IMAGE_BUTTON_CHECKOUT_EXPRESS_TITLE', 'Mon achat rapide');
-define('CSS_IMAGE_BUTTON_CHECKOUT_EXPRESS_ICON_RIGHT', '<i class="fa-solid fa-arrow-right-long"></i>');
-define('CSS_IMAGE_BUTTON_CHECKOUT_EXPRESS_ICON_LEFT', '<i class="fa-solid fa-cart-plus"></i>');
+define('CSS_IMAGE_BUTTON_CHECKOUT_EXPRESS_TEXT', 'Mein Schnellkauf');
+define('CSS_IMAGE_BUTTON_CHECKOUT_EXPRESS_TITLE', 'Mein Schnellkauf');
+define('CSS_IMAGE_BUTTON_CHECKOUT_EXPRESS_ICON_RIGHT', '<i class="fas fa-angle-right"></i>');
+define('CSS_IMAGE_BUTTON_CHECKOUT_EXPRESS_ICON_LEFT', '<i class="fas fa-cart-plus"></i>');
 
 define('CSS_IMAGE_SMALL_EXPRESS_TEXT', '');
 define('CSS_IMAGE_SMALL_EXPRESS_TITLE', 'Mon achat rapide');
 define('CSS_IMAGE_SMALL_EXPRESS_ICON_RIGHT', '');
-define('CSS_IMAGE_SMALL_EXPRESS_ICON_LEFT', '<i class="fa-solid fa-cart-plus"></i>');
+define('CSS_IMAGE_SMALL_EXPRESS_ICON_LEFT', '<i class="fas fa-cart-plus"></i>');
 
 define('CSS_IMAGE_SMALL_CART_TEXT', '');
 define('CSS_IMAGE_SMALL_CART_ICON_RIGHT', '');
-define('CSS_IMAGE_SMALL_CART_ICON_LEFT', '<i class="fa-solid fa-cart-shopping"></i>');
+define('CSS_IMAGE_SMALL_CART_ICON_LEFT', '<i class="fas fa-shopping-cart"></i>');
 
 define('CSS_IMAGE_BUTTON_BUY_NOW_TEXT', '');
 define('CSS_IMAGE_BUTTON_BUY_NOW_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_BUY_NOW_ICON_LEFT', '<i class="fa-solid fa-cart-shopping"></i>');
+define('CSS_IMAGE_BUTTON_BUY_NOW_ICON_LEFT', '<i class="fas fa-shopping-cart"></i>');
 
-define('CSS_IMAGE_BUTTON_CONTINUE_SHOPPING_ICON_RIGHT', '<i class="fa-solid fa-arrow-right-long"></i>');
+define('CSS_IMAGE_BUTTON_CONTINUE_SHOPPING_ICON_RIGHT', '<i class="fas fa-caret-right"></i>');
 define('CSS_IMAGE_BUTTON_CONTINUE_SHOPPING_ICON_LEFT', '');
 
 define('CSS_IMAGE_SMALL_EDIT_TEXT', 'Modifier');
 define('CSS_IMAGE_SMALL_EDIT_TITLE', 'Modifier');
 define('CSS_IMAGE_SMALL_EDIT_ICON_RIGHT', '');
-define('CSS_IMAGE_SMALL_EDIT_ICON_LEFT', '<i class="fa-solid fa-pen-to-square"></i>');
+define('CSS_IMAGE_SMALL_EDIT_ICON_LEFT', '<i class="far fa-edit"></i>');
 
 define('CSS_IMAGE_SMALL_DELETE_TEXT', 'Supprimer');
 define('CSS_IMAGE_SMALL_DELETE_TITLE', 'Supprimer');
 define('CSS_IMAGE_SMALL_DELETE_ICON_RIGHT', '');
-define('CSS_IMAGE_SMALL_DELETE_ICON_LEFT', '<i class="fa-regular fa-trash-can"></i>');
+define('CSS_IMAGE_SMALL_DELETE_ICON_LEFT', '<i class="far fa-trash-alt"></i>');
 
-define('CSS_IMAGE_SMALL_VIEW_TEXT', '');
 define('CSS_IMAGE_SMALL_VIEW_ICON_RIGHT', '');
-define('CSS_IMAGE_SMALL_VIEW_ICON_LEFT', '<i class="fa-solid fa-eye"></i>');
+define('CSS_IMAGE_SMALL_VIEW_ICON_LEFT', '');
 
 define('CSS_IMAGE_BUTTON_SHIPPING_OPTIONS_ICON_RIGHT', '');
 define('CSS_IMAGE_BUTTON_SHIPPING_OPTIONS_ICON_LEFT', '');
@@ -155,26 +152,24 @@ define('CSS_IMAGE_BUTTON_SHIPPING_OPTIONS_ICON_LEFT', '');
 define('CSS_IMAGE_BUTTON_REVIEWS_ICON_RIGHT', '');
 define('CSS_IMAGE_BUTTON_REVIEWS_ICON_LEFT', '');
 
-define('CSS_IMAGE_BUTTON_REDEEM_TEXT', '');
 define('CSS_IMAGE_BUTTON_REDEEM_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_REDEEM_ICON_LEFT', '<i class="fa-solid fa-gift"></i>');
+define('CSS_IMAGE_BUTTON_REDEEM_ICON_LEFT', '');
 
 define('CSS_IMAGE_BUTTON_EDIT_ACCOUNT_ICON_RIGHT', '');
 define('CSS_IMAGE_BUTTON_EDIT_ACCOUNT_ICON_LEFT', '');
 
 define('CSS_IMAGE_PRINT_ICON_RIGHT', '');
-define('CSS_IMAGE_PRINT_ICON_LEFT', '<i class="fa-solid fa-print"></i>');
+define('CSS_IMAGE_PRINT_ICON_LEFT', '<i class="fas fa-print"></i>');
 
 define('CSS_IMAGE_BUTTON_HISTORY_ICON_RIGHT', '');
 define('CSS_IMAGE_BUTTON_HISTORY_ICON_LEFT', '');
 
 define('CSS_IMAGE_BUTTON_PRODUCT_MORE_TEXT', '');
-define('CSS_IMAGE_BUTTON_PRODUCT_MORE_ICON_RIGHT', '<i class="fa-solid fa-eye"></i>');
+define('CSS_IMAGE_BUTTON_PRODUCT_MORE_ICON_RIGHT', '<i class="fas fa-chevron-circle-right"></i>');
 define('CSS_IMAGE_BUTTON_PRODUCT_MORE_ICON_LEFT', '');
 
-define('CSS_IMAGE_BUTTON_IN_WISHLIST_TEXT', '');
 define('CSS_IMAGE_BUTTON_IN_WISHLIST_ICON_RIGHT', '');
-define('CSS_IMAGE_BUTTON_IN_WISHLIST_ICON_LEFT', '<i class="fa-regular fa-heart"></i>');
+define('CSS_IMAGE_BUTTON_IN_WISHLIST_ICON_LEFT', '');
 
 define('CSS_IMAGE_BUTTON_CONFIRM_ICON_RIGHT', '');
 define('CSS_IMAGE_BUTTON_CONFIRM_ICON_LEFT', '');
@@ -210,7 +205,7 @@ define('CSS_IMAGE_BUTTON_QUICK_FIND_HEAD_ICON_RIGHT', '');
 define('CSS_IMAGE_BUTTON_QUICK_FIND_HEAD_ICON_LEFT', '');
 
 define('CSS_IMAGE_ICON_CART_TEXT', '');
-define('CSS_IMAGE_ICON_CART_ICON_RIGHT', '<i class="fa-solid fa-cart-plus"></i>');
+define('CSS_IMAGE_ICON_CART_ICON_RIGHT', '<i class="fas fa-cart-plus"></i>');
 define('CSS_IMAGE_ICON_CART_ICON_LEFT', '');
 
 define('CSS_IMAGE_EPAYPAL_DE_TEXT', '<span class="color-paypal0">Accès direct</span><span class="color-paypal2">àPayPal</span>');
@@ -220,7 +215,7 @@ define('CSS_IMAGE_EPAYPAL_DE_ICON_LEFT', '');
 define('CSS_IMAGE_SMALL_CONTINUE_TEXT', 'Sélectionner');
 define('CSS_IMAGE_SMALL_CONTINUE_TITLE', 'Sélectionner');
 define('CSS_IMAGE_SMALL_CONTINUE_ICON_RIGHT', '');
-define('CSS_IMAGE_SMALL_CONTINUE_ICON_LEFT', '<i class="fa-regular fa-square-check"></i>');
+define('CSS_IMAGE_SMALL_CONTINUE_ICON_LEFT', '<i class="far fa-check-square"></i>');
 
-define('CSS_IMAGE_BUTTON_RESULTS_ICON_RIGHT', '<i class="fa-solid fa-arrow-right-long"></i>');
+define('CSS_IMAGE_BUTTON_RESULTS_ICON_RIGHT', '<i class="fas fa-chevron-circle-right"></i>');
 define('CSS_IMAGE_BUTTON_RESULTS_ICON_LEFT', '');
